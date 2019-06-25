a01fc66e66bc504c97bbb4a97ccba23e8bf02303
David Gallardo <David@MacBook-Pro-de-David.local>
2019-06-25 18:13:00 +0200

    ios code

--- src/ios/OnSpota.h
+++ src/ios/OnSpota.h
@@ -1,9 +1,17 @@
 #import <Cordova/CDVPlugin.h>
 
+#import <OnSpotSdk/OnSpotaApi.h>
+#import <OnSpotSdk/OnSpotSdk.h>
+
 @interface OnSpota : CDVPlugin {
 }
 
 // Encabezados de las funciones del plugin
-- (void) saludar:(CDVInvokedUrlCommand*)command;
+- (void) initTracker:(CDVInvokedUrlCommand*)command;
+- (void) suspendTracker:(CDVInvokedUrlCommand*)command;
+- (void) setUserId:(CDVInvokedUrlCommand*)command;
+- (void) setApiHost:(CDVInvokedUrlCommand*)command;
+- (void) resumeTracker:(CDVInvokedUrlCommand*)command;
+
 
 @end
--- src/ios/OnSpota.m
+++ src/ios/OnSpota.m
@@ -2,22 +2,97 @@
 
 #import <Cordova/CDVAvailability.h>
 
+#import <OnSpotSdk/OnSpotaApi.h>
+#import <OnSpotSdk/OnSpotSdk.h>
+
 @implementation OnSpota
 
+OnSpotaCore *sharedInstance;
+
 - (void)pluginInitialize {
+     sharedInstance = [[OnSpotaCore alloc] init];
+    
+}
+-(void)application:(UIApplication *)application performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+    
+    [sharedInstance resumeTracker];
+    
+    //Tell the system that you ar done.
+    completionHandler(UIBackgroundFetchResultNewData);
 }
 
-- (void)saludar:(CDVInvokedUrlCommand*)command
+
+- (void)initTracker:(CDVInvokedUrlCommand*)command
 {
 
-    NSString* name = [[command arguments] objectAtIndex:0];
-    NSString* msg = [NSString stringWithFormat: @"Hola todo el...  %@", name];
-
+    NSString* sdkId = [[command arguments] objectAtIndex:0];
+    NSLog(@"%@", [NSString stringWithFormat: @"SDK Iniciado con key: %@", sdkId]);
+    
+    [sharedInstance initTracker:sdkId];
+    
     CDVPluginResult* result = [CDVPluginResult
-                               resultWithStatus:CDVCommandStatus_OK
-                               messageAsString:msg];
+                               resultWithStatus:CDVCommandStatus_OK];
 
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    
+    
 }
+- (void)suspendTracker:(CDVInvokedUrlCommand*)command
+{
+    
+    
+    [sharedInstance suspendTracker];
+    
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK];
+    
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    
+    
+}
+- (void)setUserId:(CDVInvokedUrlCommand*)command
+{
+    
+    NSString* userId = [[command arguments] objectAtIndex:0];
+    
+    [sharedInstance setUserId:userId];
+    
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK];
+    
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        
+}
+
+- (void)setApiHost:(CDVInvokedUrlCommand*)command
+{
+    
+    NSString* apiHost = [[command arguments] objectAtIndex:0];
+    
+    [sharedInstance setApiHost:apiHost];
+    
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK];
+    
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    
+}
+
+- (void)resumeTracker:(CDVInvokedUrlCommand*)command
+{
+    
+    
+    [sharedInstance resumeTracker];
+    
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK];
+    
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    
+}
+
+
+
 
 @end

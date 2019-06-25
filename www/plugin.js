cd5d5e4d376900e0aced6d6491f13303197131f5
David Gallardo <David@MacBook-Pro-de-David.local>
2019-06-25 22:41:51 +0200

    plugin methods updated

--- www/plugin.js
+++ www/plugin.js
@@ -1,12 +1,26 @@
 
 var exec = require('cordova/exec');
 
-var PLUGIN_NAME = 'MiPlugin';
+var PLUGIN_NAME = 'OnSpota';
 
-var MiPlugin = {
-  saludo: function (name, successCallback, errorCallback){
-        exec(successCallback, errorCallback, PLUGIN_NAME, "saludar", [name]);
+var OnSpota = {
+  initTracker: function (apiKey, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, PLUGIN_NAME, "initTracker", [apiKey]);
+  },
+  suspendTracker: function ( successCallback, errorCallback) {
+    exec(successCallback, errorCallback, PLUGIN_NAME, "suspendTracker", []);
+  },
+  setUserId: function ( userId, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, PLUGIN_NAME, "setUserId", [userId]);
+  },
+  setApiHost: function ( host, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, PLUGIN_NAME, "setApiHost", [host]);
+  },
+  resumeTracker: function ( successCallback, errorCallback) {
+    exec(successCallback, errorCallback, PLUGIN_NAME, "resumeTracker", []);
   }
-};
 
-module.exports = MiPlugin;
+}
+
+
+module.exports = OnSpota;
